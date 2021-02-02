f42d1207e5b5476f0462f92e3c7b808fa88eab58
AhmadRizalP <rizal10priyono11@gmail.com>
2021-02-02 10:29:20 +0700

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import React, { Component, Fragment } from "react";
+
+import Galeri from "./Pages/Galeri";
+import Cart from "./Pages/Cart";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Fragment>
+          <header>
+            <div>
+              <Link to="/galeri" className="link">
+                Galer1
+              </Link>
+              <Link to="/cart" className="link">
+                Cart
+              </Link>
+            </div>
+          </header>
+
+          <Switch>
+            <Route path="/galeri" exact component={Galeri} />
+            <Route path="/cart" exact component={Cart} />
+          </Switch>
+        </Fragment>
+      </BrowserRouter>
+    );
+  }
 }
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 export default App;
